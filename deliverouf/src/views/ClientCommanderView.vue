5bc8fe4dcf3c0ac854bab0ebc33581202f168013
Victor <74705293+ExitBoy@users.noreply.github.com>
2023-01-12 11:19:26 +0100

    Command Card
    Command Card Added + Command View + related files

---
+++ deliverouf/src/views/ClientCommanderView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="">
+      <ClientCommander/>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  import ClientCommander from '@/components/ClientCommander.vue';
+  
+  export default defineComponent({
+    name: 'ClientCommanderView',
+    components: {
+      ClientCommander,
+    },
+  });
+  </script>
+  
